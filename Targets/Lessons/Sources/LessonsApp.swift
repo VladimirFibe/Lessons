8e6d4013f9ead253995ee59d219b2de152f4480d
fibeapp <motiw@me.com>
2023-02-02 04:28:10 +0600

    Start

---
+++ Targets/Lessons/Sources/LessonsApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import LessonsUI
+
+@main
+struct LessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Targets/LessonsUI/Tests/LessonsUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class LessonsUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("LessonsUI", "LessonsUI")
+    }
+}
